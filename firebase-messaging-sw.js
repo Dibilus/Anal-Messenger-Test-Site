38f4b3db38fd49963d298e6893f8c48e32ea9245
Dibilus <100204757+Dibilus@users.noreply.github.com>
2025-09-21 12:27:18 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,8 @@
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
+// Та же конфигурация Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAE0RJ5LEQ0AQT6G05UHc15FSTilVqu0vc",
     authDomain: "anal-messenger.firebaseapp.com",
@@ -10,15 +12,18 @@ const firebaseConfig = {
     appId: "1:302872384307:web:48ef1f9af350a0d476e446"
 };
 
+// Инициализация Firebase в Service Worker
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Обработка фоновых сообщений
 messaging.onBackgroundMessage((payload) => {
+    console.log('Фоновое сообщение получено: ', payload);
+    
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon-192x192.png'
+        icon: '/icon.png' // Добавьте путь к иконке
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
